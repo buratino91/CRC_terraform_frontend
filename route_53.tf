c5c6583afc91861438e810fb9ba8995bb992205f
glen chua <glenchua@MacBook-Pro.local>
2025-01-17 20:48:08 +0800

    import frontend into terraform

--- route_53.tf
+++ route_53.tf
@@ -2,6 +2,5 @@ resource aws_route53_zone "crc_zone" {
     name = "my-b4d3-resume.com"
     force_destroy = null
     comment = "HostedZone created by Route53 Registrar"
-    #id = "Z03776072531XMRC40OR7"
     tags = {}
 }
--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,33 +1,11 @@
 resource "aws_s3_bucket" "resume_bucket" {
     bucket                      = "glens-bucket"
-    #bucket_domain_name          = var.bucket_domain_name
     bucket_prefix               = null
-    #bucket_regional_domain_name = var.bucket_regional_domain_name
-    #hosted_zone_id              = "Z3AQBSTGFYJSTF"
-    #id                          = "glens-bucket"
     object_lock_enabled         = false
-   
     tags                        = {}
     tags_all                    = {}
 
 }
 
-# resource "aws_s3_bucket_versioning" "resume_bucket_versioning" {
-#     bucket = aws_s3_bucket.resume_bucket.id
-#     versioning_configuration {
-#         status = "Disabled"
-#         mfa_delete = "Disabled"
-#     }
-# }
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "resume_bucket_encryption" {
-#     bucket = aws_s3_bucket.resume_bucket.id
-#     rule{
-#             bucket_key_enabled = false
-#             apply_server_side_encryption_by_default {
-#                 kms_master_key_id = null
-#                 sse_algorithm     = "AES256"
-#             }
-#         }
-# }
     
